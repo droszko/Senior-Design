7a99916d22c6a1f70e04b4a93119cf44d4fef16d
droszko <daniel.roszko@gmail.com>
2015-04-15 20:51:40 -0400

    4/15/2015 Update

--- Main.py
+++ Main.py
@@ -3,10 +3,13 @@
 
 import urllib2
 import xml.etree.ElementTree as ET
+import datetime
+import RPi.GPIO as GPIO
 
 # The following are global variables which are set to values that 
 # are unrealistic for weather data. This allows the program to see if
 # something is not correctly being stored.
+future = 1
 temp_c = 9999.99
 temp_f = 9999.99 
 weather = "unknown"
@@ -19,20 +22,40 @@ conditions = ["NA", "NA", "NA", "NA"]
 rain_pct = [999,999,999,999]
 
 def main():
-	wunderground()
-	return 0
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setwarnings(False)
+	GPIO.setup(5, GPIO.OUT)
+	GPIO.setup(7, GPIO.OUT)
 	
-def datafile():
+	if (getdata("run") == "0"):
+		GPIO.output(5, GPIO.LOW)
+		GPIO.output(7, GPIO.LOW)
+
+	wunderground()
+	
+#	d = datetime.datetime.now()
+#	print d.hour
+#	getdata("f")
+#	for x in range(0,future+1):
+#		if(
+	return
+	
+def getdata(westerfeld):
 	xmlfile_tree = ET.parse("Data.xml")
 	xmlfile_root = xmlfile_tree.getroot()
-	
-	mylist = ["123", "345", "567", "789"]
-	for value in xmlfile_root.iter("condition"):
-		value.text = mylist[0]
-		mylist.pop(0)
-	
+
+	if(westerfeld == "f"):
+		returnValue = int(xmlfile_root.find("./future").text)
+	elif(westerfeld == "run"):
+		returnValue = xmlfile_root.find("./firstrun").text
+		for runcheck in xmlfile_root.iter("firstrun"):
+			runcheck.text = "1"
+	else:
+		print "ERROR"
+		
 	xmlfile_tree = ET.ElementTree(xmlfile_root)
-	xmlfile_tree.write("Data.xml")
+	xmlfile_tree.write("Data.xml")	
+	return returnValue
 	
 #	The following function called wunderground will be used by the 
 #	program to obtain weather data from the wunderground api. The
@@ -48,12 +71,19 @@ def wunderground():
 		conditions_file = urllib2.urlopen(url_wunderground)
 	except urllib2.HTTPError, e:
 		print "This program has encountered an HTTPError: "
+		GPIO.output(5, GPIO.LOW)
+		GPIO.output(7, GPIO.HIGH)
 		print e.code
 		return
 	except urllib2.URLError, e:
 		print "This program has encountered an URLError: "
+		GPIO.output(5, GPIO.LOW)
+		GPIO.output(7, GPIO.HIGH)
 		print e.args
 		return
+ 	
+ 	GPIO.output(5, GPIO.HIGH)
+ 	GPIO.output(7, GPIO.LOW)
  	
  	conditions_data = conditions_file.read()	
  	conditions_file.close()
@@ -89,6 +119,7 @@ def savedata(variable):
 	
 	if(variable == "u"):
 		print "u"
+		
 	elif(variable == "w"):		
 		for condition in xmlfile_root.iter("condition"):
 			condition.text = conditions[0]
@@ -114,15 +145,14 @@ def savedata(variable):
 			rotate = rain_pct[0]
 			rain_pct.pop(0)	
 			rain_pct.append(rotate)
+		for update in xmlfile_root.iter("update"):
+			update.text = "1"
 		for wind in xmlfile_root.iter("wind_mph"):
 			wind.text = wind_mph
-		
+			
 	else:
 		return
-	
-	xmlfile_tree = ET.ElementTree(xmlfile_root)
-	xmlfile_tree.write("Data.xml")
-	
+		
 	return
 
 if __name__ == '__main__':
